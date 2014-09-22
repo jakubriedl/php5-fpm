4220bfa5fc2124779d4cc7c961f447ec6a170325
Jakub Riedl <jakub.riedl@le.cz>
2014-09-22 16:05:52 +0200

    changed ruby test version

--- recipes/install.rb
+++ recipes/install.rb
@@ -14,9 +14,8 @@
 #(_ P _ ((_ H _ ((_ P _ ((_ - _ ((_ F _ ((_ P _ ((_ M _ (_
 #  |_( )__||_( )__||_( )__||_( )__||_( )__||_( )__||_( )__|
 
-if RUBY_VERSION =~ /1.9/
+if RUBY_VERSION > "1.9"
 	Encoding.default_external = Encoding::UTF_8
-	Encoding.default_internal = Encoding::UTF_8
 end
 
 #Configure REPO for Debian 6.x
